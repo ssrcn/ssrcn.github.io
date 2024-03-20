b61663b9e5a944793a90efabb332414e6e913a24
cnlty <fanghsiu@126.com>
2024-03-20 22:24:53 +0800

    Initial commit

--- InitialCommit.sh
+++ InitialCommit.sh
@@ -1,4 +1,5 @@
-# 如果没有消息后缀，默认提交信息为 `:pencil: update content`
+#!/bin/bash
+# 如果没有消息后缀，默认提交信息为 :pencil: update content
 info=$1
 if ["$info" = ""];
 #then info="update time: $(date "+%Y-%m-%d %H:%M:%S")"
---
+++ pages/posts/hi.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title: 你好, 朋友!
+date: 2024-01-03
+updated: 2024-01-03
+aplayer: true
+categories: 随笔
+---
+
+<div style="text-align:center;font-family:KaiTi;color:red">你好, 朋友!</div>
