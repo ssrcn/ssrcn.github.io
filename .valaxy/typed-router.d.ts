4aebad4436906bd7886615bd35cdf29c44ed8430
cnlty <fanghsiu@126.com>
2024-07-22 12:27:00 +0800

    Initial commit

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -31,6 +31,7 @@ declare module 'vue-router/auto-routes' {
     '/girls/': RouteRecordInfo<'/girls/', '/girls', Record<never, never>, Record<never, never>>,
     '/links/': RouteRecordInfo<'/links/', '/links', Record<never, never>, Record<never, never>>,
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
+    '/posts/0': RouteRecordInfo<'/posts/0', '/posts/0', Record<never, never>, Record<never, never>>,
     '/posts/hi': RouteRecordInfo<'/posts/hi', '/posts/hi', Record<never, never>, Record<never, never>>,
     '/posts/mihoyo/bh3': RouteRecordInfo<'/posts/mihoyo/bh3', '/posts/mihoyo/bh3', Record<never, never>, Record<never, never>>,
     '/posts/mihoyo/sr': RouteRecordInfo<'/posts/mihoyo/sr', '/posts/mihoyo/sr', Record<never, never>, Record<never, never>>,
