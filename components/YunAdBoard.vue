1de606a68f7cf1061c01cac9f686bddd2e1d1b26
cnlty <fanghsiu@126.com>
2024-10-09 12:55:52 +0800

    Initial commit

---
+++ components/YunAdBoard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// 广告板
+</script>
+
+<template>
+  <YunCard>
+    当你为错过太阳而哭泣的时侯，<br>
+    你也要再错过群星了。
+    <!-- <RouterLink to="/posts/how-to-use-adboard">
+      如何使用广告板？
+    </RouterLink> -->
+  </YunCard>
+</template>
