6ba1109349f21fa851037568c6a2b4a913a9f740
cnlty <fanghsiu@126.com>
2024-12-13 22:15:19 +0800

    Initial commit

---
+++ components/YunFooter.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
+</script>
+
+<template>
+  <YunFooter>
+    <span>
+      <img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问"
+        src="https://static.ipw.cn/icon/ipv6-s1.svg">
+      <img style='display:inline-block;vertical-align:middle' alt="本站支持SSL安全访问"
+        src="https://static.ipw.cn/icon/ssl-s1.svg">
+    </span>
+  </YunFooter>
+</template>
