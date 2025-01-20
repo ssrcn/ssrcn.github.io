8be4eedd98d93009bb65c71d32159a65323f5c13
cnlty <fanghsiu@126.com>
2025-01-20 21:10:37 +0800

    :pencil: update content

--- InitialCommit.sh
+++ InitialCommit.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # 如果没有消息后缀，默认提交信息为 :pencil: update content
-info=$1
-if ["$info" = ""];
-#then info="update time: $(date "+%Y-%m-%d %H:%M:%S")"
-#then info="Initial commit"
-then info=":pencil: update content"
-fi
+info="${1:-:pencil: update content}"
 git add -A
 git commit -m "$info"
 git push
